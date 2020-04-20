7dcf5f1da882831bfaf26baa7a1f64f43bc84c83
chenyf <1424407762@qq.com>
2020-04-20 20:54:22 +0800

    新建账单工程

---
+++ src/main/java/com/cyf/billmanger/BillMangerApplication.java
@@ -0,0 +1,13 @@
+package com.cyf.billmanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillMangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyf/billmanger/BillMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyf.billmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
